6d1de2c58ac7711dd9ed2f12d073df96e9374e16
Rivishanka <rmrurivishanka@gmail.com>
2019-09-25 21:47:48 +0530

    commiting site manager part
    Add site managers and display them

--- SphereProFrontEnd/src/App.js
+++ SphereProFrontEnd/src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import ManageSuppliers from './UIcomponents/Admin/manageSupppliers'
 import AddSuppliers from "./UIcomponents/Admin/addSupplier";
 import Home from "./UIcomponents/Admin/homeComp";
+import ManageSiteManagers from "./UIcomponents/SiteManager/ManageSiteManagers";
+import AddSiteManagers from "./UIcomponents/SiteManager/AddSiteManagers";
 
 
 class App extends Component {
@@ -19,6 +21,8 @@ class App extends Component {
                     <Route exact path="/" component={Home}/>
                     <Route path="/suppliers" component={ManageSuppliers}/>
                     <Route path="/add" component={AddSuppliers}/>
+                    <Route path="/siteManagers" component={ManageSiteManagers}/>
+                    <Route path="/addSiteManagers" component={AddSiteManagers}/>
                     </Switch>
                 </BrowserRouter>
             </div>
